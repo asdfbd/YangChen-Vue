5843ccb1eeb19dff10d42580ba928477dc62850e
longtao <2391611102qq@gmail.com>
2026-08-14 10:14:46 +0800

    sql提交

--- sql/postgresql/ry_20260417.postgresql.sql
+++ sql/postgresql/ry_20260417.postgresql.sql
@@ -196,79 +196,79 @@ create table sys_menu
 -- ----------------------------
 -- 一级菜单
 insert into sys_menu
-values ('1', '系统管理', '0', '1', 'system', null, '', '', 1, 0, 'M', '0', '0', '', 'system', 'admin', current_timestamp, '',
+values ('1', '系统管理', '0', '1', 'system', null, '', '', 1, 0, 'M', '0', '0', '', null, 'admin', current_timestamp, '',
         null, '系统管理目录');
 insert into sys_menu
-values ('2', '系统监控', '0', '2', 'monitor', null, '', '', 1, 0, 'M', '0', '0', '', 'monitor', 'admin', current_timestamp, '',
+values ('2', '系统监控', '0', '2', 'monitor', null, '', '', 1, 0, 'M', '0', '0', '', null, 'admin', current_timestamp, '',
         null, '系统监控目录');
 insert into sys_menu
-values ('3', '系统工具', '0', '3', 'tool', null, '', '', 1, 0, 'M', '0', '0', '', 'tool', 'admin', current_timestamp, '', null,
+values ('3', '系统工具', '0', '3', 'tool', null, '', '', 1, 0, 'M', '0', '0', '', null, 'admin', current_timestamp, '', null,
         '系统工具目录');
 insert into sys_menu
-values ('4', '若依官网', '0', '4', 'http://yangchen.vip', null, '', '', 0, 0, 'M', '0', '0', '', 'guide', 'admin',
+values ('4', '若依官网', '0', '4', 'http://yangchen.vip', null, '', '', 0, 0, 'M', '0', '0', '', null, 'admin',
         current_timestamp, '', null, '若依官网地址');
 -- 二级菜单
 insert into sys_menu
-values ('100', '用户管理', '1', '1', 'user', 'system/user/index', '', '', 1, 0, 'C', '0', '0', 'system:user:list',
-        'user', 'admin', current_timestamp, '', null, '用户管理菜单');
+values ('100', '用户管理', '1', '1', 'user', '/system/user/index', '', '', 1, 0, 'C', '0', '0', 'system:user:list',
+        null, 'admin', current_timestamp, '', null, '用户管理菜单');
 insert into sys_menu
-values ('101', '角色管理', '1', '2', 'role', 'system/role/index', '', '', 1, 0, 'C', '0', '0', 'system:role:list',
-        'peoples', 'admin', current_timestamp, '', null, '角色管理菜单');
+values ('101', '角色管理', '1', '2', 'role', '/system/role/index', '', '', 1, 0, 'C', '0', '0', 'system:role:list',
+        null, 'admin', current_timestamp, '', null, '角色管理菜单');
 insert into sys_menu
-values ('102', '菜单管理', '1', '3', 'menu', 'system/menu/index', '', '', 1, 0, 'C', '0', '0', 'system:menu:list',
-        'tree-table', 'admin', current_timestamp, '', null, '菜单管理菜单');
+values ('102', '菜单管理', '1', '3', 'menu', '/system/menu/index', '', '', 1, 0, 'C', '0', '0', 'system:menu:list',
+        null, 'admin', current_timestamp, '', null, '菜单管理菜单');
 insert into sys_menu
-values ('103', '部门管理', '1', '4', 'dept', 'system/dept/index', '', '', 1, 0, 'C', '0', '0', 'system:dept:list',
-        'tree', 'admin', current_timestamp, '', null, '部门管理菜单');
+values ('103', '部门管理', '1', '4', 'dept', '/system/dept/index', '', '', 1, 0, 'C', '0', '0', 'system:dept:list',
+        null, 'admin', current_timestamp, '', null, '部门管理菜单');
 insert into sys_menu
-values ('104', '岗位管理', '1', '5', 'post', 'system/post/index', '', '', 1, 0, 'C', '0', '0', 'system:post:list',
-        'post', 'admin', current_timestamp, '', null, '岗位管理菜单');
+values ('104', '岗位管理', '1', '5', 'post', '/system/post/index', '', '', 1, 0, 'C', '0', '0', 'system:post:list',
+        null, 'admin', current_timestamp, '', null, '岗位管理菜单');
 insert into sys_menu
-values ('105', '字典管理', '1', '6', 'dict', 'system/dict/index', '', '', 1, 0, 'C', '0', '0', 'system:dict:list',
-        'dict', 'admin', current_timestamp, '', null, '字典管理菜单');
+values ('105', '字典管理', '1', '6', 'dict', '/system/dict/index', '', '', 1, 0, 'C', '0', '0', 'system:dict:list',
+        null, 'admin', current_timestamp, '', null, '字典管理菜单');
 insert into sys_menu
-values ('106', '参数设置', '1', '7', 'config', 'system/config/index', '', '', 1, 0, 'C', '0', '0', 'system:config:list',
-        'edit', 'admin', current_timestamp, '', null, '参数设置菜单');
+values ('106', '参数设置', '1', '7', 'config', '/system/config/index', '', '', 1, 0, 'C', '0', '0', 'system:config:list',
+        null, 'admin', current_timestamp, '', null, '参数设置菜单');
 insert into sys_menu
-values ('107', '通知公告', '1', '8', 'notice', 'system/notice/index', '', '', 1, 0, 'C', '0', '0', 'system:notice:list',
-        'message', 'admin', current_timestamp, '', null, '通知公告菜单');
+values ('107', '通知公告', '1', '8', 'notice', '/system/notice/index', '', '', 1, 0, 'C', '0', '0', 'system:notice:list',
+        null, 'admin', current_timestamp, '', null, '通知公告菜单');
 insert into sys_menu
-values ('108', '日志管理', '1', '9', 'log', '', '', '', 1, 0, 'M', '0', '0', '', 'log', 'admin', current_timestamp, '', null,
+values ('108', '日志管理', '1', '9', 'log', '', '', '', 1, 0, 'M', '0', '0', '', null, 'admin', current_timestamp, '', null,
         '日志管理菜单');
 insert into sys_menu
-values ('109', '在线用户', '2', '1', 'online', 'monitor/online/index', '', '', 1, 0, 'C', '0', '0',
-        'monitor:online:list', 'online', 'admin', current_timestamp, '', null, '在线用户菜单');
+values ('109', '在线用户', '2', '1', 'online', '/monitor/online/index', '', '', 1, 0, 'C', '0', '0',
+        'monitor:online:list', null, 'admin', current_timestamp, '', null, '在线用户菜单');
 insert into sys_menu
-values ('110', '定时任务', '2', '2', 'job', 'monitor/job/index', '', '', 1, 0, 'C', '0', '0', 'monitor:job:list', 'job',
+values ('110', '定时任务', '2', '2', 'job', '/monitor/job/index', '', '', 1, 0, 'C', '0', '0', 'monitor:job:list', null,
         'admin', current_timestamp, '', null, '定时任务菜单');
 insert into sys_menu
-values ('111', '数据监控', '2', '3', 'druid', 'monitor/druid/index', '', '', 1, 0, 'C', '0', '0', 'monitor:druid:list',
-        'druid', 'admin', current_timestamp, '', null, '数据监控菜单');
+values ('111', '数据监控', '2', '3', 'druid', '/monitor/druid/index', '', '', 1, 0, 'C', '0', '0', 'monitor:druid:list',
+        null, 'admin', current_timestamp, '', null, '数据监控菜单');
 insert into sys_menu
-values ('112', '服务监控', '2', '4', 'server', 'monitor/server/index', '', '', 1, 0, 'C', '0', '0',
-        'monitor:server:list', 'server', 'admin', current_timestamp, '', null, '服务监控菜单');
+values ('112', '服务监控', '2', '4', 'server', '/monitor/server/index', '', '', 1, 0, 'C', '0', '0',
+        'monitor:server:list', null, 'admin', current_timestamp, '', null, '服务监控菜单');
 insert into sys_menu
-values ('113', '缓存监控', '2', '5', 'cache', 'monitor/cache/index', '', '', 1, 0, 'C', '0', '0', 'monitor:cache:list',
-        'redis', 'admin', current_timestamp, '', null, '缓存监控菜单');
+values ('113', '缓存监控', '2', '5', 'cache', '/monitor/cache/index', '', '', 1, 0, 'C', '0', '0', 'monitor:cache:list',
+        null, 'admin', current_timestamp, '', null, '缓存监控菜单');
 insert into sys_menu
-values ('114', '缓存列表', '2', '6', 'cacheList', 'monitor/cache/list', '', '', 1, 0, 'C', '0', '0',
-        'monitor:cache:list', 'redis-list', 'admin', current_timestamp, '', null, '缓存列表菜单');
+values ('114', '缓存列表', '2', '6', 'cacheList', '/monitor/cache/list', '', '', 1, 0, 'C', '0', '0',
+        'monitor:cache:list', null, 'admin', current_timestamp, '', null, '缓存列表菜单');
 insert into sys_menu
-values ('115', '表单构建', '3', '1', 'build', 'tool/build/index', '', '', 1, 0, 'C', '0', '0', 'tool:build:list',
-        'build', 'admin', current_timestamp, '', null, '表单构建菜单');
+values ('115', '表单构建', '3', '1', 'build', '/tool/build/index', '', '', 1, 0, 'C', '0', '0', 'tool:build:list',
+        null, 'admin', current_timestamp, '', null, '表单构建菜单');
 insert into sys_menu
-values ('116', '代码生成', '3', '2', 'gen', 'tool/gen/index', '', '', 1, 0, 'C', '0', '0', 'tool:gen:list', 'code',
+values ('116', '代码生成', '3', '2', 'gen', '/tool/gen/index', '', '', 1, 0, 'C', '0', '0', 'tool:gen:list', null,
         'admin', current_timestamp, '', null, '代码生成菜单');
 insert into sys_menu
-values ('117', '系统接口', '3', '3', 'swagger', 'tool/swagger/index', '', '', 1, 0, 'C', '0', '0', 'tool:swagger:list',
-        'swagger', 'admin', current_timestamp, '', null, '系统接口菜单');
+values ('117', '系统接口', '3', '3', 'swagger', '/tool/swagger/index', '', '', 1, 0, 'C', '0', '0', 'tool:swagger:list',
+        null, 'admin', current_timestamp, '', null, '系统接口菜单');
 -- 三级菜单
 insert into sys_menu
-values ('500', '操作日志', '108', '1', 'operlog', 'monitor/operlog/index', '', '', 1, 0, 'C', '0', '0',
-        'monitor:operlog:list', 'form', 'admin', current_timestamp, '', null, '操作日志菜单');
+values ('500', '操作日志', '108', '1', 'operlog', '/monitor/operlog/index', '', '', 1, 0, 'C', '0', '0',
+        'monitor:operlog:list', null, 'admin', current_timestamp, '', null, '操作日志菜单');
 insert into sys_menu
-values ('501', '登录日志', '108', '2', 'logininfor', 'monitor/logininfor/index', '', '', 1, 0, 'C', '0', '0',
-        'monitor:logininfor:list', 'logininfor', 'admin', current_timestamp, '', null, '登录日志菜单');
+values ('501', '登录日志', '108', '2', 'logininfor', '/monitor/logininfor/index', '', '', 1, 0, 'C', '0', '0',
+        'monitor:logininfor:list', null, 'admin', current_timestamp, '', null, '登录日志菜单');
 -- 用户管理按钮
 insert into sys_menu
 values ('1000', '用户查询', '100', '1', '', '', '', '', 1, 0, 'F', '0', '0', 'system:user:query', '#', 'admin',
